c204737d1f904d4be743862013dbecbb1d5f7bc3
Evan Martin <martine@danga.com>
2011-03-15 02:37:15 -0700

    remove pnm code from test file

--- sfpng_test.c
+++ sfpng_test.c
@@ -4,30 +4,9 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-static void row_func(void* context,
-                     sfpng_decoder* decoder,
-                     int row,
-                     const void* buf,
-                     size_t bytes) {
-  if (row == 0) {
-    printf("P3\n");
-    printf("%d %d\n",
-           sfpng_decoder_get_width(decoder),
-           sfpng_decoder_get_height(decoder));
-    printf("255\n");
-  }
-
-  const uint8_t* buf_bytes = buf;
-  int x;
-  for (x = 0; x < bytes; ++x)
-    printf("%d ", buf_bytes[x]);
-  printf("\n");
-}
-
 int main(int argc, char* argv[]) {
   FILE* f = fopen(argv[1], "rb");
   sfpng_decoder* decoder = sfpng_decoder_new();
-  sfpng_decoder_set_row_func(decoder, row_func);
   char buf[4096];
   size_t len;
   while ((len = fread(buf, 1, 10, f)) > 0) {
