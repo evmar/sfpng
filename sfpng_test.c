91d0b6afb501af59d5915b3fb7cfbd8232f1521e
Evan Martin <martine@danga.com>
2011-03-15 11:00:23 -0700

    write trailing empty block

--- sfpng_test.c
+++ sfpng_test.c
@@ -21,12 +21,14 @@ int main(int argc, char* argv[]) {
 
   char buf[4096];
   size_t len;
-  while ((len = fread(buf, 1, 10, f)) > 0) {
+  while ((len = fread(buf, 1, 10, f)) >= 0) {
     sfpng_status status = sfpng_decoder_write(decoder, buf, len);
     if (status != SFPNG_SUCCESS) {
       printf("decode error %d\n", status);
       return 1;
     }
+    if (len == 0)
+      break;
   }
   int width = sfpng_decoder_get_width(decoder);
   int height = sfpng_decoder_get_height(decoder);
