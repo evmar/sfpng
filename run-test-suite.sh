25f601bab67fb4ee633d8de2626c4c153dba8c0d
Evan Martin <martine@danga.com>
2011-03-16 13:54:59 -0700

    split test suite

--- run-test-suite.sh
+++ run-test-suite.sh
@@ -3,8 +3,8 @@
 valgrind="valgrind --leak-check=full --quiet --error-exitcode=2"
 valgrind=
 
-for f in testsuite/*.png; do
-    echo -n "$f: "
+for f in testsuite/*/*.png; do
+    echo -n "${f#testsuite/}: "
     if diff -q <($valgrind ./libpng-dumper $f 2>&1) \
                <($valgrind ./sfpng-dumper $f 2>&1) >/dev/null; then
         echo 'PASS'
