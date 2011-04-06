3bb68ef1ae627a418365d688437f88aad062d1d2
Evan Martin <martine@danga.com>
2011-04-06 23:07:08 +0300

    tempfiles for test suite

--- run-test-suite.sh
+++ run-test-suite.sh
@@ -3,16 +3,21 @@
 valgrind="valgrind --leak-check=full --quiet --error-exitcode=2"
 valgrind=
 
+sfpng_output=$(mktemp sfpng.XXXXXXXXXX)
+libpng_output=$(mktemp libpng.XXXXXXXXXX)
+trap "rm -f $sfpng_output $libpng_output" EXIT
+
 for f in testsuite/*/*.png; do
     echo -n "$f: "
-    if diff -q <($valgrind ./libpng-dumper $f 2>&1) \
-               <($valgrind ./sfpng-dumper $f 2>&1) >/dev/null; then
+    $valgrind ./libpng-dumper $f 2>&1 > $libpng_output
+    $valgrind ./sfpng-dumper $f 2>&1 > $sfpng_output
+
+    if diff -q $libpng_output $sfpng_output; then
         echo 'PASS'
     else
         exit=$?
         echo 'FAIL'
-        diff -U5 <($valgrind ./libpng-dumper $f 2>&1) \
-                 <($valgrind ./sfpng-dumper $f 2>&1)
+        diff -U5 $libpng_output $sfpng_output
         exit 1
     fi
 done
