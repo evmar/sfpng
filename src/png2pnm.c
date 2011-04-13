9b6f4b30907e929a8b03758263debde60fd126df
Evan Martin <martine@danga.com>
2011-04-13 11:01:42 -0700

    update png2pnm

--- src/png2pnm.c
+++ src/png2pnm.c
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+uint8_t* g_transform_buf = NULL;
+
 static void row_func(sfpng_decoder* decoder,
                      int row,
                      const uint8_t* buf,
@@ -14,25 +16,18 @@ static void row_func(sfpng_decoder* decoder,
            sfpng_decoder_get_width(decoder),
            sfpng_decoder_get_height(decoder));
     printf("255\n");
+
+    g_transform_buf = malloc(sfpng_decoder_get_width(decoder) * 4);
   }
 
+  sfpng_decoder_transform(decoder, buf, g_transform_buf);
+
   int x;
-  switch (sfpng_decoder_get_color_type(decoder)) {
-  case SFPNG_COLOR_TRUECOLOR:
-    for (x = 0; x < len; x += 3)
-      printf("%d %d %d ", buf[x], buf[x+1], buf[x+2]);
-    break;
-  case SFPNG_COLOR_TRUECOLOR_ALPHA:
-    for (x = 0; x < len; x += 4) {
-      int a = buf[x+3];
-      printf("%d %d %d ",
-             buf[x] * a / 255,
-             buf[x+1] * a / 255,
-             buf[x+2] * a / 255);
-    }
-    break;
-  default:
-    fprintf(stderr, "color format unhandled\n");
+  for (x = 0; x < sfpng_decoder_get_width(decoder) * 4; x += 4) {
+    printf("%d %d %d ",
+           g_transform_buf[x],
+           g_transform_buf[x+1],
+           g_transform_buf[x+2]);
   }
   printf("\n");
 }
@@ -50,12 +45,14 @@ int main(int argc, char* argv[]) {
 
   char buf[4096];
   size_t len;
-  while ((len = fread(buf, 1, sizeof(buf), f)) > 0) {
+  while ((len = fread(buf, 1, sizeof(buf), f)) >= 0) {
     sfpng_status status = sfpng_decoder_write(decoder, buf, len);
     if (status != SFPNG_SUCCESS) {
       fprintf(stderr, "decode error %d\n", status);
       return 1;
     }
+    if (len == 0)
+      break;
   }
   sfpng_decoder_free(decoder);
 
