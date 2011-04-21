d62e69adc80532df18ae3dd43169e36af7c321b6
Evan Martin <martine@danga.com>
2011-04-20 22:35:48 -0700

    use zlib crc32

--- testsuite/pngforge.py
+++ testsuite/pngforge.py
@@ -13,31 +13,12 @@ def rgb(r, g, b):
 def sig():
     return struct.pack('8B', 137, 80, 78, 71, 13, 10, 26, 10)
 
-def make_crc_table():
-    table = []
-    for n in range(256):
-        c = long(n)
-        for k in range(8):
-            if c & 1:
-                c = 0xedb88320L ^ (c >> 1)
-            else:
-                c = c >> 1
-        table.append(c)
-    return table
-g_crc_table = make_crc_table()
-
-def compute_crc(data):
-    c = 0xFFFFFFFFL
-    for b in data:
-        c = g_crc_table[(c ^ ord(b)) & 0xFF] ^ (c >> 8)
-    return c ^ 0xFFFFFFFFL
-
 def chunk(chunk_type, data='', crc=None, length=None):
     if length is None:
         length = len(data)
     body = chunk_type + data
     if crc is None:
-        crc = compute_crc(body)
+        crc = zlib.crc32(body) & 0xFFFFFFFF
     return struct.pack('>L', length) + body + struct.pack('>L', crc)
 
 def ihdr(width, height, depth=8, color_type=COLOR_TRUE,
